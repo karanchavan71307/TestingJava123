25596a64239b33cad77453b93e683f514e1a74f1
karanchavan71307 <karanchavan70307@gmail.com>
2022-11-06 07:12:01 -0800

    adding the java program additions

---
+++ ArithMaticOperations/src/com/first/Additions.java
@@ -0,0 +1,17 @@
+package com.first;
+
+public class Additions {
+
+	public static void main(String[] args) {
+		
+		
+		int a=10;
+		int b=20;
+		
+		int c=a+b;
+		
+		System.out.println("addtions of the two Number : "+c);
+		
+	}
+
+}
